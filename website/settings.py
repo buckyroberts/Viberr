ecc7d82bf0f4465b923454fe7c868c48b11dabe9
GreenJoey <esyray15@gmail.com>
2016-12-31 09:56:48 +0530

    Made the template dir in pythonic way

--- website/settings.py
+++ website/settings.py
@@ -39,7 +39,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/', ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
