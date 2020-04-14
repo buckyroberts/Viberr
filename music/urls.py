ceee077ac9410238ec735eb90ae4baa368b55a68
Shubham Upadhyay <upadhyayshubham0503@gmail.com>
2020-04-14 08:11:19 -0700

    Django version bump

--- music/urls.py
+++ music/urls.py
@@ -1,19 +1,20 @@
 from django.conf.urls import url
+from django.urls import path, re_path
 from . import views
 
 app_name = 'music'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^register/$', views.register, name='register'),
-    url(r'^login_user/$', views.login_user, name='login_user'),
-    url(r'^logout_user/$', views.logout_user, name='logout_user'),
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<song_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
-    url(r'^songs/(?P<filter_by>[a-zA_Z]+)/$', views.songs, name='songs'),
-    url(r'^create_album/$', views.create_album, name='create_album'),
-    url(r'^(?P<album_id>[0-9]+)/create_song/$', views.create_song, name='create_song'),
-    url(r'^(?P<album_id>[0-9]+)/delete_song/(?P<song_id>[0-9]+)/$', views.delete_song, name='delete_song'),
-    url(r'^(?P<album_id>[0-9]+)/favorite_album/$', views.favorite_album, name='favorite_album'),
-    url(r'^(?P<album_id>[0-9]+)/delete_album/$', views.delete_album, name='delete_album'),
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login_user/', views.login_user, name='login_user'),
+    path('logout_user/', views.logout_user, name='logout_user'),
+    path('<int:album_id>/', views.detail, name='detail'),
+    path('<int:song_id>/favorite/', views.favorite, name='favorite'),
+    re_path(r'^songs/(?P<filter_by>[a-zA_Z]+)/$', views.songs, name='songs'),
+    path('create_album/', views.create_album, name='create_album'),
+    path('<int:album_id>/create_song/', views.create_song, name='create_song'),
+    path('<int:album_id>/delete_song/<int:song_id>/', views.delete_song, name='delete_song'),
+    path('<int:album_id>/favorite_album/', views.favorite_album, name='favorite_album'),
+    path('<int:album_id>/delete_album/', views.delete_album, name='delete_album'),
 ]
--- website/urls.py
+++ website/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^music/', include('music.urls')),
-    url(r'^', include('music.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^music/', include('music.urls')),
+    #re_path(r'^', include('music.urls')),
 ]
 
 if settings.DEBUG:
